07be2e12cd12dba4a7428b61bea0806ff5415e30
pengfei <liu.pengfei@hotmail.fr>
2021-06-25 19:23:57 +0200

    finish column class

--- sparkcommonfunc/data_transformation/ColumnClassExp.py
+++ sparkcommonfunc/data_transformation/ColumnClassExp.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession, Row
 from pyspark.sql.functions import lit
-from pyspark.sql.functions import col, expr
+from pyspark.sql.functions import col, expr, when
+from pyspark.sql.types import StructField, StructType, StringType, ArrayType, MapType
 
 # from pyspark.sql import
 
@@ -157,7 +158,11 @@ bool value, we can use them inside filter.
                                   boolean value.
 - contains(arg): Check if String contains in another string. Returns boolean value.
 - startswith(arg): Check if String starts with another string. Returns boolean value
-- endswith(other): Check if String starts with another string. Returns boolean value	
+- endswith(other): Check if String starts with another string. Returns boolean value
+- isNotNull(): Returns True if the current expression is NOT null.
+- isNull(): Returns True if the current expression is null.	
+- isin(*cols):	A boolean expression that is evaluated to true if the value of this expression is contained by the 
+                evaluated values of the arguments(a list of values).
 """
 
 
@@ -179,6 +184,120 @@ def exp8(df):
     df.withColumn("starts_with", df.fname.startswith("Tom")).show()
     print("exp8 output: check name ends with and")
     df.withColumn("starts_with", df.fname.endswith("and")).show()
+
+    # check null/non null
+    print("exp8 output: check id column contains null value or not")
+    df.withColumn("null", df.id.isNull()).show()
+    print("exp8 output: check id column contains not null value or not")
+    df.withColumn("not_null", df.id.isNotNull()).show()
+
+    # check a column value in a list
+    print("exp8 output: check id column is in string list of 100, 200")
+    li1 = ["100", "200"]
+    df1 = df.withColumn("inList", df.id.isin(li1))
+    print("exp8 output: check id column is in int list ")
+    df.withColumn("inList", df.id.cast("int").isin([100, 300])).show()
+    # Reverse the bool result in a filter, note the null value row is dropped too
+    print("exp8 output: filter id column not in the list ")
+    df.filter(~df.id.isin(li1)).show()
+
+
+""" Exp9: Substring a string column
+- substr(startPos, length): Return a Column which is a substring of the column. startPos starts with 0.
+"""
+
+
+def exp9(df):
+    print("exp9 output: sub string example ")
+    df.withColumn("substr", df.fname.substr(0, 3)).show()
+
+
+""" Exp10: Fuzz search
+- like(other): Similar to SQL like expression.
+- rlike(other): Similar to SQL RLIKE expression (LIKE with Regex).
+"""
+
+
+def exp10(spark):
+    data = [("James", "fun|stuff "),
+            ("Ann", "dancing is fun"),
+            ("Tom Cruise", "you have stuff"),
+            ("Tom Brand", None)]
+
+    columns = ["fname", "phrase"]
+    df = spark.createDataFrame(data, columns)
+    print("exp10 output:  like example ")
+    df.withColumn("like", df.fname.like("%om%")).show()
+    print("exp10 output:  rlike example ")
+    df.withColumn("rlike", df.fname.rlike("Tom")).show()
+    print("exp10 output:  rlike with or condition example ")
+    # "|".join(keywords) is equivalent to keywords.mkString("|") in scala
+    keywords = ["Tom", "James"]
+    df.withColumn("rlike", df.fname.rlike("|".join(keywords))).show()
+
+    # if you have special character like |%$* in target string. You need to protect them to avoid regular expression
+    # translate them. For example, below will translate and we will see three rows returns true
+    print("exp10 output:  rlike with protected special character ")
+    df.withColumn("special", df.phrase.rlike("fun|stuff")).show()
+    print("exp10 output:  rlike with protected special character ")
+    # After protection, only one row returns true
+    df.withColumn("special", df.phrase.rlike("fun\\|stuff")).show()
+
+
+""" Exp11: Modify each row value by using When otherwise
+- when(condition, value),otherwise(value): Similar to SQL CASE WHEN, Executes a list of conditions and returns 
+                              one of multiple possible result expressions.
+
+Following example, change the gender column value "M"->"Male", "F"->"Female", None->"", otherwise->origin_column_value
+"""
+
+
+def exp11(df):
+    print("exp11 output:  when, otherwise example ")
+    df.select(df.fname, df.lname, when(df.gender == "M", "Male")
+              .when(df.gender == "F", "Female")
+              .when(df.gender == None, "")
+              .otherwise(df.gender).alias("new_gender")
+              ).show()
+
+
+""" Exp12: Get/set struct field value of a column 
+We can use . to get field implicitly. Or we can use the following methode to get them explicitly
+- getField(name): Returns a field by name in a StructField and by key in Map.
+- getItem(key): Returns a values from Map/Key at the provided position.
+- dropFields(*fieldNames)	Used to drops fields in StructType by name.
+- withField(fieldName, col)	An expression that adds/replaces a field in StructType by name.
+"""
+
+
+def exp12(spark):
+    data = [(("James", "Bond"), ["Java", "C#"], {'hair': 'black', 'eye': 'brown'}),
+            (("Ann", "Varsa"), [".NET", "Python"], {'hair': 'brown', 'eye': 'black'}),
+            (("Tom Cruise", ""), ["Python", "Scala"], {'hair': 'red', 'eye': 'grey'}),
+            (("Tom Brand", None), ["Perl", "Ruby"], {'hair': 'black', 'eye': 'blue'})]
+
+    schema = StructType([
+        StructField('name', StructType([
+            StructField('fname', StringType(), True),
+            StructField('lname', StringType(), True)])),
+        StructField('languages', ArrayType(StringType()), True),
+        StructField('properties', MapType(StringType(), StringType()), True)
+    ])
+    df = spark.createDataFrame(data, schema)
+    df.printSchema()
+
+    # Get field by using implicit method
+    print("exp12 output:  get field value by using . ")
+    df.select(df.name.fname.alias("first_name"), df.name.lname.alias("last_name")).show()
+    print("exp12 output:  get field value by using getField ")
+    df.select(col("name").getField("fname").alias("first_name"), df.name.getField("lname").alias("last_name")).show()
+
+    # create a new column new_name by using dropFields to drop fname field,
+    print("exp12 output:  drop field fname ")
+    df.withColumn("new_name", df['name'].dropFields("fname")).show()
+    # Create a new column complete_name by using withField to add a new field middle_name
+    print("exp12 output:  add field value to column name")
+    df.withColumn("complete_name", df['name'].withField("middle_name", df.properties.eye)).show(truncate=False)
 
 
 def main():
@@ -217,7 +336,19 @@ def main():
     # exp7(df)
 
     # run exp8
-    exp8(df)
+    # exp8(df)
+
+    # run exp9
+    # exp9(df)
+
+    # run exp10
+    # exp10(spark)
+
+    # run exp11
+    # exp11(df)
+
+    # run exp12
+    exp12(spark)
 
 
 if __name__ == "__main__":
