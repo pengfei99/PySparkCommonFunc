4827eabe4a8b523924fc971def2e3f8c1e494023
pengfei <liu.pengfei@hotmail.fr>
2021-07-14 21:21:39 +0200

    update split data

--- PySparkBasics/L01_PySpark_Intro/S06_SplitDataToMultipleGroups.py
+++ PySparkBasics/L01_PySpark_Intro/S06_SplitDataToMultipleGroups.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import col
 
 """
 When you train a model, you often needs to split your data set into 3 sub data set:
@@ -6,16 +7,29 @@ When you train a model, you often needs to split your data set into 3 sub data s
 - validation
 - test
 
-We have two methods that can do this job easily:
+We have three methods that can split a dataframe:
 - randomSplit(weights, seed): It takes two arguments 
           -- weights: a list of doubles as weights with which to split the DataFrame. Weights will be normalized if 
                       they don’t sum up to 1.0.
           -- seed: int, optional. If you give a fix value, spark should produce the same split result every time.
 
 - sample(withReplacement, fraction, seed=None): 
-          -- withReplacement: bool, optional. Sample with replacement or not (default False).
+          -- withReplacement: bool, optional. Sample with replacement or not (default False). If you want duplicates 
+                              rows in a split, set it to true 
           -- fraction: float, optional. Fraction of rows to generate, range [0.0, 1.0].
           -- seed: int, optional. Seed for sampling (default a random seed).
+
+- sampleBy(col, fractions, seed=None): sample rows by filtering specific values of a row. Check exp2()
+          -- col: Column 
+          -- fractions: dict. sampling fraction for each stratum. If a stratum is not specified, we treat its 
+                        fraction as zero.
+          -- seed: int, optional. random seed
+          
+For rdd, we have two methods:
+- sample(withReplacement, fraction, seed=None): The sample syntaxe and argument as for dataframe
+- takeSample(self, withReplacement, number, seed=None): Unlike other methods are transformation, this takeSample() is 
+           an action. Hence you may have out-of-memory error in your driver, if you return too much data results.
+            
 
 Important note: Even you set seed to a fix value in your randomSplit or sample, your split may not be consistent. 
                 These inconsistencies may not happen on every run, but it will happen.
@@ -38,36 +52,77 @@ If the generated number is in this range, then accept, otherwise reject.
 """
 
 
+# sample example
 def exp1(df: DataFrame):
     seed = 888
     df1 = df.sample(0.1, seed)
     print("Exp1: 10% sample has {} rows".format(df1.count()))
 
 
+# sampleBy example
 def exp2(df: DataFrame):
+    print("exp2: show transformed data frame")
+    # we transform the value of id to 0,1,2
+    dataset = df.select((col("id") % 3).alias("key"))
+    dataset.show(10)
+    print("exp2: show the count of the transformed data frame")
+    dataset.groupBy("key").count().orderBy("key").show()
+    # we use column key to filter rows, if the value is 0, we take all. if the value is 1, we take 30%, if the value
+    # is 2, we take 20 percent
+    sampled = dataset.sampleBy("key", fractions={0: 1, 2: 0.2, 1: 0.3}, seed=0)
+    print("exp2: show the sampleBy result data frame")
+    sampled.show(10)
+    print("exp2: show the count of the result data frame")
+    sampled.groupBy("key").count().orderBy("key").show()
+
+
+# random split example
+def exp3(df: DataFrame):
     seed = 666
     train, validation, test = df.randomSplit([0.7, 0.2, 0.1], seed)
     train_num = train.count()
     val_num = validation.count()
     test_num = test.count()
-    print("Exp2: train sample has {} rows".format(train_num))
-    print("Exp2: validation sample has {} rows".format(val_num))
-    print("Exp2: test sample has {} rows".format(test_num))
-    print("Exp2: Sum of sample rows: {}".format(test_num + val_num + train_num))
+    print("Exp3: train sample has {} rows".format(train_num))
+    print("Exp3: validation sample has {} rows".format(val_num))
+    print("Exp3: test sample has {} rows".format(test_num))
+    print("Exp3: Sum of sample rows: {}".format(test_num + val_num + train_num))
+
+
+# rdd sample and takeSample example
+def exp4(spark: SparkSession):
+    rdd = spark.sparkContext.range(0, 100)
+    print("Exp4 get sample without duplicates by using sample()")
+    print(rdd.sample(False, 0.1, 0).collect())
+    print("Exp4 get sample with duplicates by using sample()")
+    print(rdd.sample(True, 0.3, 0).collect())
+
+    # takeSample does not need collect to start the calculation
+    # note we dont use fraction, but directly the sample number that we want
+    print("Exp4 get sample without duplicates by using takeSample()")
+    print(rdd.takeSample(False, 10, 0))
+    print("Exp4 get sample with duplicates by using takeSample()")
+    print(rdd.takeSample(True, 30, 0))
 
 
 def main():
     spark = SparkSession.builder.master("local[2]").appName("SplitDataToMultipleGroups").getOrCreate()
-    df: DataFrame = spark.range(0, 10000)
+    df: DataFrame = spark.range(0, 100)
     print("Source data frame has {} rows".format(df.count()))
     df.printSchema()
     df.show(5)
 
     # run exp1
-    exp1(df)
+    # exp1(df)
 
     # run exp2
-    exp2(df)
+    # exp2(df)
+
+    # run exp3
+    # exp3(df)
+
+    # run exp4
+    exp4(spark)
 
 
 if __name__ == "__main__":
