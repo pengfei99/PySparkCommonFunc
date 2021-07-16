e3a94fb755dac1c1bca2a7f5f5d310bf3ede7913
pengfei <liu.pengfei@hotmail.fr>
2021-07-16 11:55:10 +0200

    finish aggregation function

--- PySparkBasics/L02_DataFrame/S03_GroupByAndAggregation/02_AggregationFunction.py
+++ PySparkBasics/L02_DataFrame/S03_GroupByAndAggregation/02_AggregationFunction.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import count, countDistinct, approx_count_distinct, avg, min, max, mean, collect_list, \
-    collect_set
+    collect_set, grouping, first, last, sum, sumDistinct, skewness, kurtosis, stddev, stddev_samp, stddev_pop, \
+    variance, var_samp, var_pop
 
 """
 Aggregate functions operate on a group of rows and calculate a single return value for every group.
@@ -9,18 +10,13 @@ In previous GroupByExp, we have seen some aggregation example. In this tutorial,
 aggregation functions in spark. The full list in alphabetic order:
 
 - approx_count_distinct, count, countDistinct
-- avg, max, min, mean
+- avg/mean, max, min, 
 - collect_list, collect_set
-- grouping
-- first
-- last
-- kurtosis
-- skewness
-- stddev
-- stddev_samp
-- stddev_pop
-- sum
-- sumDistinct
+- grouping : Check if a column is created by aggregation function or not, returns 1 for aggregated, 0 for not aggregated
+- first,last
+- sum, sumDistinct
+- kurtosis, skewness
+- stddev, stddev_samp, stddev_pop
 - variance, var_samp, var_pop
 """
 
@@ -101,6 +97,96 @@ def exp3(df: DataFrame):
     df.select(collect_set("salary")).show(truncate=False)
 
 
+""" Exp4
+Can't make it work, always get "grouping() can only be used with GroupingSets/Cube/Rollup;" error
+"""
+
+
+def exp4(df: DataFrame):
+    # we can't use withColumn to create a column by using aggregation function
+    df1 = df.groupBy("department").avg("salary")
+    df1.show()
+    df1.select(grouping("avg(salary)")).show()
+
+
+""" Exp5 
+- first(): it returns the first element in a column when ignoreNulls is set to true, 
+          it returns the first non-null element.
+- last(): returns the last element in a column. when ignoreNulls is set to true, 
+          it returns the last non-null element.
+"""
+
+
+def exp5(df: DataFrame):
+    df.select(first("salary"), last("department")).show()
+
+
+""" Exp6
+sum: returns the sum of all values in a column.
+sumDistinct: returns the sum of all distinct values in a column.
+"""
+
+
+def exp6(df: DataFrame):
+    df.select(sum("salary"), sumDistinct("salary")).show()
+
+
+""" Exp7
+When we do descriptive analysis, we want to know the skewness of a distribution. If a distribution is not skewed, which
+is a normal distribution, then we want to know the Kurtosis(峰度) of the distribution.
+
+Skewness is a measure of symmetry, or more precisely, the lack of symmetry. A distribution, or data set, is symmetric 
+if it looks the same to the left and right of the centre point. 
+
+Kurtosis measures whether your dataset is heavy-tailed or light-tailed compared to a normal distribution. 
+Data sets with high kurtosis have heavy tails and more outliers and data sets with low kurtosis tend to have 
+light tails and fewer outliers.
+"""
+
+
+def exp7(df: DataFrame):
+    # note the salary distribution is negatively skewed, it means mean<median<Mode
+    # The kurtosis is negative too, it means the distribution is very flat and dispersed
+    df.select(skewness("salary"), kurtosis("salary")).show()
+
+
+""" Exp 8
+Standard deviation is a measure of the amount of variation or dispersion of a set of values.
+- A low standard deviation indicates that the values tend to be close to the mean (also called the expected value) 
+     of the set
+- A high standard deviation indicates that the values are spread out over a wider range.
+
+Spark provides three methods:
+- stddev(): alias for stddev_samp.
+- stddev_samp(): returns the unbiased sample standard deviation of the expression in a group.
+- stddev_pop(): returns population standard deviation of the expression in a group.
+
+"""
+
+
+def exp8(df: DataFrame):
+    df.select(stddev("salary"), stddev_samp("salary"), stddev_pop("salary")).show()
+
+
+""" Exp9
+Variance is also a measure of the amount of variation or dispersion of a set of values. The difference with standard
+deviation is that square functions are used during the calculation because they weight outliers more heavily than 
+points that are near to the mean. This prevents that differences above the mean neutralize those below the mean.
+
+But the square function makes the Variance change the unit of measurement of the original data. For example, a column
+contains centimeter values. Your variance would be in squared centimeters and therefore not the best measurement. 
+
+Spark provides three methods:
+- variance(): alias for var_samp.
+- var_samp(): returns the unbiased sample variance of the values in a group.
+- var_pop(): returns the population variance of the values in a group.
+"""
+
+
+def exp9(df: DataFrame):
+    df.select(variance("salary"), var_samp("salary"), var_pop("salary")).show()
+
+
 def main():
     spark = SparkSession.builder.master("local[2]").appName("AggregationFunction").getOrCreate()
     data = [("Alice", "Sales", 3000),
@@ -127,7 +213,25 @@ def main():
     # exp2(df)
 
     # run exp3
-    exp3(df)
+    # exp3(df)
+
+    # run exp4
+    # exp4(df)
+
+    # run exp5
+    # exp5(df)
+
+    # run exp6
+    # exp6(df)
+
+    # run exp7
+    # exp7(df)
+
+    # run exp8
+    # exp8(df)
+
+    # run exp9
+    exp9(df)
 
 
 if __name__ == "__main__":
